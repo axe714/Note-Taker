cea0690ff144e137800b0c8b7cc744282194a092
Axe <axearzadon@gmail.com>
2022-06-07 18:18:35 -0700

    Added path + get route in server.js file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
+const path = require('path')
 const app = express();
 
 //uses the PORT that heroku uses, OR defaults to 3001
@@ -16,3 +17,7 @@ app.use("/", apiRoutes);
 app.listen(PORT, () =>
   console.log(`Listening on PORT ${PORT}. View it on http://localhost:${PORT}`)
 );
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+})
